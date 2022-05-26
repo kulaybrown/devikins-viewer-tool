ef2ae399e63b77952b9a45655519a6b81586cae1
Angelo Lagumbay <angelol@istackmanila.com>
2022-05-26 19:22:31 +0800

    style: find parent page

--- assets/scripts/components/FindParent.js
+++ assets/scripts/components/FindParent.js
@@ -13,15 +13,11 @@ class FindParent {
 
 addListeners() {
     this.$selectButton.on("click", this.handleClickFindParent.bind(this));
-    // this.$optionItem.on("click", this.handleSelect.bind(this));
-    // $($selectButton).on("click", (event) => {
-    //   const $target = $(event.target);
-    //   if (!$target.closest(".select-dropdown").is(this.$element.get(0))) {
-    //     this.$element.removeClass("select-dropdown--open");
-    //   }
-    // });
-
-    
+    this.$selectInput.on("keypress", function(e) {
+      if (e.key === "Enter") {
+        $(e.target).parent(".find-parent-action").find("button").trigger("click")
+      }
+    })
   }
 
   handleGetChildID() {
@@ -94,6 +90,7 @@ addListeners() {
               },
               success: function (resultx) {
                 const parentContainer2 = $(".parent-container");
+                
 
                 // const grandparents = '<div class="generation" id="grand-parents"><div class="pair"></div></div>'
                 // parentContainer.append(grandparents)
@@ -106,44 +103,46 @@ addListeners() {
                   parentContainer2.find($('#grand-parents .pair')[index]).append(`<div class='grandparent-item card-m' ${g1 && `devi-id='${g1}'`} devi-rarity=''>
                     <div style='background-image: url(https://img.devikins.com/${handleImgIndexPath(g1)}/${g1}.png);'>
                       <div class='gene-cont'>
-                        <div class='gene-mouth'>gene-mouth</div>
-                        <div class='gene-eye'>gene-eye</div>
-                        <div class='gene-ear'>gene-ear</div>
-                        <div class='gene-hair'>gene-hair</div>
-                        <div class='gene-horn'>gene-horn</div>
+                        <div class='gene gene-mouth'></div>
+                        <div class='gene gene-eye'></div>
+                        <div class='gene gene-ear'></div>
+                        <div class='gene gene-hair'></div>
+                        <div class='gene gene-horn'></div>
                       </div>
-                      <div>${g1}</div>
+                      <div class='devi-id'>${g1}</div>
+                      <div class='devi-oa'></div>
                       <div class='aff-cont'>
-                        <div class='aff-vit'>aff-vit</div>
-                        <div class='aff-power'>aff-power</div>
-                        <div class='aff-for'>aff-for</div>
-                        <div class='aff-agi'>aff-agi</div>
-                        <div class='aff-san'>aff-san</div>
+                        <div class='aff-vit'><span></span></div>
+                        <div class='aff-pow'><span></span></div>
+                        <div class='aff-for'><span></span></div>
+                        <div class='aff-agi'><span></span></div>
+                        <div class='aff-san'><span></span></div>
                       </div>
                     </div>
                   </div>`);
                   parentContainer2.find($('#grand-parents .pair')[index]).append(`<div class='grandparent-item card-f' ${g2 && `devi-id='${g2}'`} devi-rarity=''>
                     <div style='background-image: url(https://img.devikins.com/${handleImgIndexPath(g2)}/${g2}.png);'>
                       <div class='gene-cont'>
-                        <div class='gene-mouth'>gene-mouth</div>
-                        <div class='gene-eye'>gene-eye</div>
-                        <div class='gene-ear'>gene-ear</div>
-                        <div class='gene-hair'>gene-hair</div>
-                        <div class='gene-horn'>gene-horn</div>
+                        <div class='gene gene-mouth'></div>
+                        <div class='gene gene-eye'></div>
+                        <div class='gene gene-ear'></div>
+                        <div class='gene gene-hair'></div>
+                        <div class='gene gene-horn'></div>
                       </div>
-                      <div>${g2}</div>
+                      <div class='devi-id'>${g2}</div>
+                      <div class='devi-oa'></div>
                       <div class='aff-cont'>
-                        <div class='aff-vit'>aff-vit</div>
-                        <div class='aff-power'>aff-power</div>
-                        <div class='aff-for'>aff-for</div>
-                        <div class='aff-agi'>aff-agi</div>
-                        <div class='aff-san'>aff-san</div>
+                        <div class='aff-vit'><span></span></div>
+                        <div class='aff-pow'><span></span></div>
+                        <div class='aff-for'><span></span></div>
+                        <div class='aff-agi'><span></span></div>
+                        <div class='aff-san'><span></span></div>
                       </div>
                     </div>
                   </div>`);
                 } else {
-                  parentContainer2.find($('#grand-parents .pair')[index]).append(`<div class='grandparent-item card-m no-parents'>no-parent</div>`);
-                  parentContainer2.find($('#grand-parents .pair')[index]).append(`<div class='grandparent-item card-f no-parents'>no-parent</div>`);
+                  parentContainer2.find($('#grand-parents .pair')[index]).append(`<div class='grandparent-item card-m no-parents'></div>`);
+                  parentContainer2.find($('#grand-parents .pair')[index]).append(`<div class='grandparent-item card-f no-parents'></div>`);
                 }
               },
               error: function () {
@@ -158,19 +157,20 @@ addListeners() {
           parentContainer.find('#parents .pair').append(`<div class='parent-item card-m' ${p1 && `devi-id='${p1}'`} devi-rarity=''>
             <div style='background-image: url(https://img.devikins.com/${handleImgIndexPath(p1)}/${p1}.png);'>
               <div class='gene-cont'>
-                <div class='gene-mouth'>gene-mouth</div>
-                <div class='gene-eye'>gene-eye</div>
-                <div class='gene-ear'>gene-ear</div>
-                <div class='gene-hair'>gene-hair</div>
-                <div class='gene-horn'>gene-horn</div>
+                <div class='gene gene-mouth'></div>
+                <div class='gene gene-eye'></div>
+                <div class='gene gene-ear'></div>
+                <div class='gene gene-hair'></div>
+                <div class='gene gene-horn'></div>
               </div>
-              <div>${p1}</div>
+              <div class='devi-id'>${p1}</div>
+              <div class='devi-oa'></div>
               <div class='aff-cont'>
-                <div class='aff-vit'>aff-vit</div>
-                <div class='aff-power'>aff-power</div>
-                <div class='aff-for'>aff-for</div>
-                <div class='aff-agi'>aff-agi</div>
-                <div class='aff-san'>aff-san</div>
+                <div class='aff-vit'><span></span></div>
+                <div class='aff-pow'><span></span></div>
+                <div class='aff-for'><span></span></div>
+                <div class='aff-agi'><span></span></div>
+                <div class='aff-san'><span></span></div>
               </div>
             </div>
           
@@ -178,19 +178,20 @@ addListeners() {
           parentContainer.find('#parents .pair').append(`<div class='parent-item card-f' ${p2 && `devi-id='${p2}'`} devi-rarity=''>
             <div style='background-image: url(https://img.devikins.com/${handleImgIndexPath(p2)}/${p2}.png);'>
               <div class='gene-cont'>
-                <div class='gene-mouth'>gene-mouth</div>
-                <div class='gene-eye'>gene-eye</div>
-                <div class='gene-ear'>gene-ear</div>
-                <div class='gene-hair'>gene-hair</div>
-                <div class='gene-horn'>gene-horn</div>
+                <div class='gene gene-mouth'></div>
+                <div class='gene gene-eye'></div>
+                <div class='gene gene-ear'></div>
+                <div class='gene gene-hair'></div>
+                <div class='gene gene-horn'></div>
               </div>
-              <div>${p2}</div>
+              <div class='devi-id'>${p2}</div>
+              <div class='devi-oa'></div>
               <div class='aff-cont'>
-                <div class='aff-vit'>aff-vit</div>
-                <div class='aff-power'>aff-power</div>
-                <div class='aff-for'>aff-for</div>
-                <div class='aff-agi'>aff-agi</div>
-                <div class='aff-san'>aff-san</div>
+                <div class='aff-vit'><span></span></div>
+                <div class='aff-pow'><span></span></div>
+                <div class='aff-for'><span></span></div>
+                <div class='aff-agi'><span></span></div>
+                <div class='aff-san'><span></span></div>
               </div>
             </div>
           </div>`);
@@ -199,19 +200,20 @@ addListeners() {
           const child = `<div class='generation' devi-id='${deviChildID}' id='child' devi-rarity=''>
           <div class='card-m' style='background-image: url(https://img.devikins.com/${handleImgIndexPath(deviChildID)}/${deviChildID}.png);'>
             <div class='gene-cont'>
-              <div class='gene-mouth'>gene-mouth</div>
-              <div class='gene-eye'>gene-eye</div>
-              <div class='gene-ear'>gene-ear</div>
-              <div class='gene-hair'>gene-hair</div>
-              <div class='gene-horn'>gene-horn</div>
+              <div class='gene gene-mouth'></div>
+              <div class='gene gene-eye'></div>
+              <div class='gene gene-ear'></div>
+              <div class='gene gene-hair'></div>
+              <div class='gene gene-horn'></div>
             </div>
-            <div>${deviChildID}</div>
+            <div class='devi-id'>${deviChildID}</div>
+            <div class='devi-oa'></div>
             <div class='aff-cont'>
-              <div class='aff-vit'>aff-vit</div>
-              <div class='aff-power'>aff-power</div>
-              <div class='aff-for'>aff-for</div>
-              <div class='aff-agi'>aff-agi</div>
-              <div class='aff-san'>aff-san</div>
+              <div class='aff-vit'><span></span></div>
+              <div class='aff-pow'><span></span></div>
+              <div class='aff-for'><span></span></div>
+              <div class='aff-agi'><span></span></div>
+              <div class='aff-san'><span></span></div>
             </div>
           </div>
         </div>`;
@@ -275,6 +277,20 @@ addListeners() {
             const parentContainer = $(".parent-container");
             const selectdeviId = $(parentContainer.find('[devi-id]'));
 
+            const geneMouth = selectdeviId.find(".gene-mouth");
+            const geneEye = selectdeviId.find(".gene-eye");
+            const geneEar = selectdeviId.find(".gene-ear");
+            const geneHair = selectdeviId.find(".gene-hair");
+            const geneHorn = selectdeviId.find(".gene-horn");
+
+            const affVit = selectdeviId.find(".aff-vit");
+            const affPow = selectdeviId.find(".aff-pow");
+            const affFor = selectdeviId.find(".aff-for");
+            const affAgi = selectdeviId.find(".aff-agi");
+            const affSan = selectdeviId.find(".aff-san");
+
+            const deviOA = selectdeviId.find(".devi-oa");
+
             for (let index2 = 0; index2 < selectdeviId.length; index2++) {
               let id = $(selectdeviId[index2]).attr('devi-id');
               // console.log(id + " xxxxx")
@@ -282,15 +298,37 @@ addListeners() {
               if(id === JSON.stringify(resultxx.SequenceCounter)) {
                 // console.log(resultxx.OverallAffinity)
                 $(selectdeviId[index2]).attr('devi-rarity', handleRarity(resultxx.OverallAffinity));
+                $(geneMouth[index2]).attr("data-gene", handleGenes(resultxx.Mouth.Tag))
+                $(geneEye[index2]).attr("data-gene", handleGenes(resultxx.Eyes.Tag))
+                $(geneEar[index2]).attr("data-gene", handleGenes(resultxx.Ears.Tag))
+                $(geneHair[index2]).attr("data-gene", handleGenes(resultxx.Hair.Tag))
+                $(geneHorn[index2]).attr("data-gene", handleGenes(resultxx.Horns.Tag))
+
+                $(affVit[index2]).attr("data-aff", handleAffinity(resultxx.VitAffinity))
+                $(affPow[index2]).attr("data-aff", handleAffinity(resultxx.PowAffinity))
+                $(affFor[index2]).attr("data-aff", handleAffinity(resultxx.ForAffinity))
+                $(affAgi[index2]).attr("data-aff", handleAffinity(resultxx.AgiAffinity))
+                $(affSan[index2]).attr("data-aff", handleAffinity(resultxx.SanAffinity))
+
+                $(affVit[index2]).find("span").text(resultxx.VitAffinity);
+                $(affPow[index2]).find("span").text(resultxx.PowAffinity);
+                $(affFor[index2]).find("span").text(resultxx.ForAffinity);
+                $(affAgi[index2]).find("span").text(resultxx.AgiAffinity);
+                $(affSan[index2]).find("span").text(resultxx.SanAffinity);
+
+                $(deviOA[index2]).text(resultxx.OverallAffinity);
+
               }
             }
-
+            
             // console.log("hoy")
             // console.log(JSON.stringify(resultxx.SequenceCounter) + " heloo");
             // console.log(deviFamily)
             // console.log("adasd")
             // console.log(parentContainer)
             // parentContainer.append('<span>hello </span>')
+            
+
           },
           error: function () {
             console.log("error");
@@ -331,6 +369,9 @@ addListeners() {
       else if(id < 999999) {
         x = 9;
       }
+      else if(id < 1999999) {
+        x = 10;
+      }
       return x;
     }
 
@@ -349,6 +390,49 @@ addListeners() {
       }
       return x;
     }
+
+    function handleGenes(gene) {
+      let x = "dominant";
+      if(gene === 0) {
+        x = "dominant";
+      } else if(gene === 1) {
+        x = "co-dominant";
+      } else if(gene === 2) {
+        x = "recessive";
+      } else if(gene === 3) {
+        x = "mutated";
+      } else if(gene === 4) {
+        x = "unique";
+      } else {
+        x = "bonded";
+      }
+      return x;
+    }
+
+    function handleAffinity(aff) {
+      let x = "common";
+      if(aff < 4) {
+        x = "common";
+      } else if(aff < 6) {
+        x = "uncommon";
+      } else if(aff < 8) {
+        x = "rare";
+      } else if(aff < 10) {
+        x = "mythic";
+      } else {
+        x = "eldritch";
+      }
+      return x;
+    }
+
+    function handleGranParentsHide() {
+      const parentContainer = $(".parent-container");
+      const gp = parentContainer.find("#grand-parents");
+
+      if(document.querySelectorAll(".no-parents").length === 4) {
+        gp.css("display", "none");
+      }
+    }
   }
 
   handleRenderParentID(p1, p2) {
@@ -356,7 +440,7 @@ addListeners() {
     $(this.$parent2).text(p2)
   }
 
-
+  
 //   handleSelect(e) {
 //     const selectValue = $(e.target).text();
 //     this.$select.text(selectValue);
